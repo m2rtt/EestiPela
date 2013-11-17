217d4275254f4adfb1b366d342ccabd0341e9e7d
m2rtt <m2rt.sessman@gmail.com>
2013-11-17 17:55:54 +0200

    totally new shit
    võid uuendada esialgu vana, üritan siin vähe ümber korraldada seda
    süsteemi

---
+++ alterMain.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+# http://zetcode.com/gui/tkinter/introduction/ tkinter tutorial
+
+# kujundus ja GUI teha PySide ja photoshopiga: http://qt-project.org/wiki/Category:LanguageBindings::PySide
+# http://zetcode.com/gui/pysidetutorial/firstprograms/
+
+from random import randint
+import sys
+from PySide.QtGui import *
+from PySide.QtCore import *
+from PySide.QtDeclarative import QDeclarativeView
+
+
+
+def kysimusevaldkond(fail, kys_id):
+    faililist = ["tyhi","kyssad\\ajalugu.lol","kyssad\\kultuur.lol","kyssad\\loodus.lol",
+                 "kyssad\\geo.lol","kyssad\\sport.lol","kyssad\\varia.lol"]
+    f = open(faililist[fail])
+    kysimuslist = []
+    vastuslist = []
+    for line in f:
+        kys = line[line.find('|')+1:line.find('>')]
+        kysimuslist.append(kys)
+        vastus = line[line.find('>')+1:].strip()
+        vastuslist.append(vastus)
+    f.close()
+    return kysimuslist[kys_id], vastuslist[kys_id]
+    # valdkonna usage:
+    # kysimusevaldkond(valdkond, kĆ¼simuse id)
+    # valdkonnad:
+    # 1 - ajalugu, 2 - kultuur, 3 - loodus, 4 - geograafia, 5 - sport, 6 - varia
+
+
+
+class Programm(QWidget):
+
+    @Slot()
+    def wat(self):
+        x, y = kysimusevaldkond(randint(1,6),randint(1,10))
+        if len(x) > 77:
+            return x[:77]+"\n "+x[77:], y
+        else:
+            return x, y
+        #do sth
+    on_aaa = Signal()
+    aaa = Property(str,wat,notify=on_aaa)
+    test = "kas github n2itab"
+
+    @Slot()
+    def check_answer(self):
+        if y in vastus.SelectAll():
+            return True
+        else:
+            return False
+    on_bbb = Signal()
+    bbb = Property(bool,check_answer,notify=on_bbb)
+
+    def __init__(self):
+        super(Programm, self).__init__()
+        
+        self.initUI()
+
+
+
+        
+
+    
+
+
+def center(proge):
+        
+    qr = proge.frameGeometry()
+    cp = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(cp)
+    proge.move(qr.topLeft())
+
+def main():
+    
+    app = QApplication(sys.argv)
+    view = QDeclarativeView()
+    # Create an URL to the QML file
+    url = QUrl('MyElement.qml')
+    # Set the QML file and show
+    view.setSource(url)
+    view.rootContext().setContextProperty('proge', view)
+    center(view)
+    view.setFixedSize(500,340)
+    tiitel = "Eesti mang"
+    view.setWindowTitle(tiitel) #millegipärast täpitäht ei tööta
+    view.show()
+    app.exec_()
+
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
